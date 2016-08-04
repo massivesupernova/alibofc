1acd544ef45988d5f583b56050ed18f11cb3de74
massivesupernova <flw_r@163.com>
2016-08-05 01:08:31 +0800

    Update filesplit.c

--- util/filesplit.c
+++ util/filesplit.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 int main(int argc, char** argv) {
   if (argc < 2 || argv == 0) {
-    printf("[E] Usage: filesplit <file>\n");
+    printf("[E] Invalid command line parameters.\nUsage: filesplit <large_file_path>\n");
     return 1;
   }
   char* filepath_str = argv[1];
   if (filepath_str == 0 || filepath_str[0] == 0) {
-    printf("[E] Invalid input file path\n");
+    printf("[E] Invalid input file path.\n");
     return 1;
   }
   FILE* infile = fopen(filepath_str, "rb");
   if (infile == 0) {
-    printf("[E] Open file to read failed: %s\n", filepath_str);
+    printf("[E] Open file to read failed %s: %s.\n", filepath_str, strerror(errno));
     return 1;
   }
   FILE* outfile = 0;
@@ -22,15 +24,9 @@ int main(int argc, char** argv) {
   unsigned long maxfilebytes = (unsigned long)(1024*1024) * (unsigned long)4000;
   for (;;) {
     if (sprintf(outname, "./part.%ld", fileno++) < 11) {
-      printf("[E] Generate output file name failed\n");
+      printf("[E] Compose output file name failed.\n");
       goto close_infile_and_return_fail;
     }
-    outfile = fopen(outname, "wb");
-    if (outfile == 0) {
-      printf("[E] Open file to write failed: %s\n", outname);
-      goto close_infile_and_return_fail;
-    }
-    printf("%s:\n", outname);
     unsigned long i = 0;
     unsigned long prev_mb = 0;
     unsigned long mb = 0;
@@ -41,18 +37,28 @@ int main(int argc, char** argv) {
       if ((ch = fgetc(infile)) == EOF) {
         break;
       }
+      if (i == 0) {
+        if ((outfile = fopen(outname, "wb")) == 0) {
+          printf("[E] Open file to write failed %s: %s.\n", outname, strerror(errno));
+          goto close_infile_and_return_fail;
+        }
+        printf("%s:\n", outname);
+      }
       if (fputc(ch, outfile) == EOF) {
-        printf("[E] Write file failed %ld %s\n", i + 1, outname);
+        printf("[E] Write file %s failed, already written %ldB, current fail char %02x: %s.\n", outname, i, ch, strerror(errno));
         fclose(outfile);
         goto close_infile_and_return_fail;
       }
       prev_mb = mb;
       mb = (i + 1) / (1024 * 1024);
       if (mb != prev_mb && (mb % 100) == 0) {
-        printf("written 100MB... %d\n", ++logcount);
+        printf("Written 100MB... %d\n", ++logcount);
       }
     }
-    fclose(outfile);
+    if (outfile) {
+      fclose(outfile);
+      outfile = 0;
+    }
     if (ch == EOF) {
       // current "i" (added 1 and fgetc EOF) is the total written bytes
       i = i - mb * 1024 * 1024; // remain bytes for kb
@@ -60,9 +66,14 @@ int main(int argc, char** argv) {
       i = i - kb * 1024;        // remain bytes for b
       b = i % 1024;             // calc b
       if (mb != 0 || kb != 0 || b != 0) {
-        printf("written %ldMB %ldKB %ldB... %d\n", mb % 100, kb, b, ++logcount);
+        printf("Written %ldMB %ldKB %ldB... %d\n", mb % 100, kb, b, ++logcount);
       }
-      printf("written completed.\n");
+      if (logcount == 0) {
+        printf("Completed, nothing written.\n");
+      }
+      else {
+        printf("Written completed.\n");
+      }
       break;
     }
   }
